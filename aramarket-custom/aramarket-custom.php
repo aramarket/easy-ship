9bf50b493c4c4b15641de090ebc7953f1fa3face
Akash <aramarket.in@gmail.com>
2024-07-26 18:18:09 +0530

    order update twice in a day working good

--- aramarket-custom/aramarket-custom.php
+++ aramarket-custom/aramarket-custom.php
@@ -7,7 +7,8 @@ if (!class_exists('AramarketCustom')) {
     class AramarketCustom {
         
 		private $orderTracking;
-
+        private $batch_size = 50; // Set your batch size here
+		
         public function __construct() {
 			$this->orderTracking = new ESOrderTracking();
             // You can add your hooks here
@@ -25,56 +26,74 @@ if (!class_exists('AramarketCustom')) {
 			}
 		}
 		
-		public function autoCheckAndUpdateOrderStatus() {
-			file_put_contents(ABSPATH . 'wp-content/my_custom_cron_log.txt', 'Cron job update stutus within executed at ' . current_time('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
-			$args = array(
-				'post_type'      => 'shop_order',
-				'post_status'    => array('wc-pending-pickup', 'wc-intransit'),
-				'posts_per_page' => -1,
-			);
-			$orders = new WP_Query($args);
-			$order_IDs = wp_list_pluck($orders->posts, 'ID');
-			foreach ($order_IDs as $order_ID) {
-				$order = wc_get_order( $order_ID );	
-				$order_status = $order->get_status(); 
-// 				$oldStatus = $this->orderTracking->getDeliveryStatusCode($order_status); //vs given below functions
+        public function autoCheckAndUpdateOrderStatus() {
+            file_put_contents(ABSPATH . 'wp-content/my_custom_cron_log.txt', 'Cron job update status within executed at ' . current_time('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
 
-				if($order_status == 'pending-pickup'){
-					$oldStatus = 2;  // 2 for pending pickup
-				} else if($order_status == 'intransit'){
-					$oldStatus = 3; // 3 for intransit
-				} else {
-					$oldStatus = 1; // 1 do nothing
-				}
-				$newStatus = $this->orderTracking->es_tracking_page($order_ID, true);
-				if($newStatus != 1){
-					if($oldStatus != $newStatus) {
-						switch ($newStatus) {
-// 							case 0: // No need for cancelled order as of now
-// 								$order->update_status( 'cancelled', 'ES: ');
-// 								$order->save();
-// 								break;
-// 							case 2: // No need for pending pickup order as of now
-// 								$order->update_status( 'pending-pickup', 'ES: ');
-// 								$order->save();
-// 								break;
-							case 3:
-								$order->update_status( 'intransit', 'ES: ');
-								$order->save();
-								break;
-							case 4:
-								$order->update_status( 'completed', 'ES: ');
-								$order->save();
-								break;
-							case 5:
-								$order->update_status( 'returnintransit', 'ES: ');
-								$order->save();
-								break;
-						}
-					}
-				}
-			}
-		}
+            $args = array(
+                'post_type'      => 'shop_order',
+                'post_status'    => array('wc-pending-pickup', 'wc-intransit'),
+                'posts_per_page' => $this->batch_size,
+                'paged'          => 1
+            );
+
+            $orders = new WP_Query($args);
+
+            while ($orders->have_posts()) {
+                $orders->the_post();
+                $order_ID = get_the_ID();
+                $this->process_order($order_ID);
+            }
+
+            wp_reset_postdata();
+
+            if ($orders->max_num_pages > 1) {
+                for ($i = 2; $i <= $orders->max_num_pages; $i++) {
+                    $args['paged'] = $i;
+                    $orders = new WP_Query($args);
+
+                    while ($orders->have_posts()) {
+                        $orders->the_post();
+                        $order_ID = get_the_ID();
+                        $this->process_order($order_ID);
+                    }
+
+                    wp_reset_postdata();
+                }
+            }
+        }
+		
+		private function process_order($order_ID) {
+            $order = wc_get_order($order_ID);    
+            $order_status = $order->get_status(); 
+
+            if ($order_status == 'pending-pickup') {
+                $oldStatus = 2;  // 2 for pending pickup
+            } else if ($order_status == 'intransit') {
+                $oldStatus = 3; // 3 for intransit
+            } else {
+                $oldStatus = 1; // 1 do nothing
+            }
+
+            $newStatus = $this->orderTracking->es_tracking_page($order_ID, true);
+            if ($newStatus != 1) {
+                if ($oldStatus != $newStatus) {
+                    switch ($newStatus) {
+                        case 3:
+                            $order->update_status('intransit', 'ES: ');
+                            $order->save();
+                            break;
+                        case 4:
+                            $order->update_status('completed', 'ES: ');
+                            $order->save();
+                            break;
+                        case 5:
+                            $order->update_status('returnintransit', 'ES: ');
+                            $order->save();
+                            break;
+                    }
+                }
+            }
+        }
 		
         public function check_and_redirect_url() {
             // Get the current URL
